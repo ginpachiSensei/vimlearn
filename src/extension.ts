67926728d8fe3c5afeab81120b890655d24cf69d
satyam <satyamrocker1234@gmail.com>
2022-07-30 19:23:45 +0530

    moved from js to typescript

--- src/extension.ts
+++ src/extension.ts
@@ -1,23 +1,22 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-const vscode = require("vscode");
+import * as vscode from "vscode";
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-
-/**
- * @param {vscode.ExtensionContext} context
- */
-function activate(context) {
+export function activate(context: vscode.ExtensionContext) {
   // Use the console to output diagnostic information (console.log) and errors (console.error)
   // This line of code will only be executed once when your extension is activated
-  console.log('Congratulations, your extension "vimlearn" is now active!');
+  console.log(
+    'Congratulations, your extension "helloworld-sample" is now active!'
+  );
+
   // The command has been defined in the package.json file
-  // Now provide the implementation of the command with  registerCommand
+  // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand(
+  const disposable: vscode.Disposable = vscode.commands.registerCommand(
     "vimlearn.vimLearn",
-    function () {
+    () => {
       vscode.commands.executeCommand(
         "vscode.open",
         vscode.Uri.joinPath(context.extensionUri, "res/cheatsheet.txt"),
@@ -25,13 +24,8 @@ function activate(context) {
       );
     }
   );
+
   context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
-function deactivate() {}
-
-module.exports = {
-  activate,
-  deactivate,
-};
+export function deactivate() {}
